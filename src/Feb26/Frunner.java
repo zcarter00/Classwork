00824083e7ed8b5ff4519e22d9ec29241f0c2fb7
zcarter00 <mczachcarter@gmail.com>
2018-02-27 09:17:20 -0600

    compiled our wits into one and tested compareTo without error

---
+++ src/Feb26/Frunner.java
@@ -0,0 +1,11 @@
+package Feb26;
+
+public class Frunner
+{
+    public static void main(String[] args)
+    {
+        Fraction f1 = new Fraction(1,4);
+        Fraction f2 = new Fraction(1,3);
+        System.out.println(f1.compareTo(f2));
+    }
+}
