29dbd9e6db29b96337f13bbeda34968cd87d39ad
zcarter00 <mczachcarter@gmail.com>
2018-02-27 09:03:33 -0600

    used magic wand to make instance variable constructors and compareTo

--- src/Feb26/Fraction.java
+++ src/Feb26/Fraction.java
@@ -2,5 +2,26 @@ package Feb26;
 
 public class Fraction implements Comparable<Fraction>
 {
+    private int num;
+    private int denom;
 
+    public Fraction(int num, int denom)
+    {
+        this.num = num;
+        this.denom = denom;
+        simplify();
+    }
+    @Override
+    public int compareTo(Fraction o)
+    {
+        int diff = this.num*o.denom - o.num*this.denom;
+        if(diff>0) return 1;
+        else if(diff<0) return -1;
+        return 0;
+    }
+
+    public void simplify()
+    {
+
+    }
 }
