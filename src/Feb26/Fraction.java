3c9d28721bfd670843b066f3a423794cd8955de8
zcarter00 <mczachcarter@gmail.com>
2018-02-28 08:55:57 -0600

    me n mr goebel made a equals method

--- src/Feb26/Fraction.java
+++ src/Feb26/Fraction.java
@@ -12,16 +12,27 @@ public class Fraction implements Comparable<Fraction>
         simplify();
     }
     @Override
-    public int compareTo(Fraction o)
-    {
-        int diff = this.num*o.denom - o.num*this.denom;
-        if(diff>0) return 1;
-        else if(diff<0) return -1;
+    public int compareTo(Fraction o) {
+        int diff = this.num * o.denom - o.num * this.denom;
+        if (diff > 0) return 1;
+        else if (diff < 0) return -1;
         return 0;
     }
+
+    @Override
+    public boolean equals(Fraction o)
+    {
+        if(o instanceof Fraction)
+        {
+            return this.compareTo((Fraction)o) == 0;
+        }
+        return false;
+    }
+
 
     public void simplify()
     {
 
     }
+
 }
--- src/Feb26/Frunner.java
+++ src/Feb26/Frunner.java
@@ -7,5 +7,8 @@ public class Frunner
         Fraction f1 = new Fraction(1,4);
         Fraction f2 = new Fraction(1,3);
         System.out.println(f1.compareTo(f2));
+        String s1 = "Frog";
+        String s2 = "Frog";
+        System.out.println(s1==s2);
     }
 }
